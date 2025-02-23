4f0273aa1030ae59122d6618143394e141dc7d68
jakobsaadbye <31799001+jakobsaadbye@users.noreply.github.com>
2025-02-24 00:11:15 +0100

    Export of react stuff also

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,9 @@
-export * from "./src/sqlitedb.ts"
-export * from "./src/change.ts"
-export * from "./src/syncer.ts"
+export * from "./index.ts"
 
-export * from "./src/sqlitedbWrapper.ts"
+// export * from "./src/sqlitedb.ts"
+// export * from "./src/change.ts"
+// export * from "./src/syncer.ts"
+
+// export * from "./src/sqlitedbWrapper.ts"
+
+// export * from "./src/react/index.ts"
